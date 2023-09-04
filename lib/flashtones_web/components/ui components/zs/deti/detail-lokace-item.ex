77d9172a6db276b6ee7c29916e52a01e83856a71
Jorik Chase <jorikchase@gmail.com>
2023-09-04 18:29:17 +0200

    detail lokace

---
+++ lib/flashtones_web/components/ui components/zs/deti/detail-lokace-item.ex
@@ -0,0 +1,37 @@
+defmodule DetailLokaceItem do
+  use Phoenix.Component
+
+  def detailLokaceItemFontana(assigns) do
+    ~H"""
+    <style>
+      .detail-lokace-item{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px;
+      }
+      .detail-lokace-item-column{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-start;
+        padding: 0 0 0 30px;
+      }
+    </style>
+    <div class="detail-lokace-item">
+      <img src="/images/plavani/hotel-fontana.png">
+      <div class="detail-lokace-item-column">
+        <h4>
+          Hotel Fontana
+        </h4>
+        <p>
+          Leží v Krkonošském národním parku, kousek od Sněžky, přímo na sjezdovce Hnědý vrch. Hodiny plavání probíhají ve vyhřívaném krytém hotelovém bazénu.
+        </p>
+        <a href="/zs/deti/courses/location">více informací</a>
+      </div>
+    </div>
+    """
+  end
+
+end
---
+++ lib/flashtones_web/components/ui components/zs/deti/detail-lokace.ex
@@ -0,0 +1,25 @@
+defmodule DetailLokace do
+  use Phoenix.Component
+
+  def detailLokace(assigns) do
+    ~H"""
+    <style>
+      .detail-lokace{
+        display:flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+    </style>
+    <div class="detail-lokace">
+      <DetailLokaceItem.detailLokaceItemFontana />
+      <DetailLokaceItem.detailLokaceItemFontana />
+      <DetailLokaceItem.detailLokaceItemFontana />
+      <DetailLokaceItem.detailLokaceItemFontana />
+      <DetailLokaceItem.detailLokaceItemFontana />
+      <DetailLokaceItem.detailLokaceItemFontana />
+    </div>
+    """
+  end
+
+end
