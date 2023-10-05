36257655c60c95b06ef84741b72c97879363bf93
jorikchase <jorikchase@gmail.com>
2023-10-05 16:27:43 +0200

    doma general

--- lib/flashtones_web/components/ui components/zs/deti/general.ex
+++ lib/flashtones_web/components/ui components/zs/deti/general.ex
@@ -9,7 +9,7 @@ defmodule General do
         flex-direction: column;
         justify-content: space-evenly;
         padding: 60px;
-        background: linear-gradient(1deg, #c5f0b8, #ffffff);
+        background: linear-gradient(1deg, #c5f0b8, #ffffff3c);
         background-size: 400% 400%;
 
         -webkit-animation: EnviroGradient 6s ease infinite;
@@ -48,7 +48,7 @@ defmodule General do
         flex-direction: column;
         justify-content: space-evenly;
         padding: 60px;
-        background: linear-gradient(1deg, #eaf0b8, #ffffff);
+        background: linear-gradient(1deg, #eaf0b8, #ffffff3c);
         background-size: 400% 400%;
 
         -webkit-animation: VyletyGradient 6s ease infinite;
@@ -75,6 +75,44 @@ defmodule General do
       <div class="projekt"><h3>JAKÝ JE KONCEPT ZŠ VÝLETŮ?</h3><p>Pořádáme jednodenní a vícedenní vzdělávací výlety pro školy a veřejnost. Stěžejní téma našich zájezdů je příroda a její ochrana a regionální historie, v neposlední řadě je pro nás podstatné i tmelení kolektivu skupiny. V případě zájmu jsme schopni pro vás vytvořit výlety přesně na míru.</p></div>
       <div class="prubeh"><p>Již v autobuse, případně v cílovém místě po příjezdu je k dispozici náš delegát, který účastníkům pomůže zorientovat se a následně jim poutavě předává veškeré informace a zajímavosti. Nepředkládáme dětem nudná data, vše probíhá interaktivní formou a každý zájezd obsahuje mnohá zpestření ústředního tématu.</p></div>
       <div class="program"><p>Zajišťujeme dopravu škol přímo na místo, naše moderní autobusy jsou vybaveny bezpečnostními pásy a splňují předepsané emisní normy Evropské unie.</p></div>
+    </div>
+    """
+  end
+  def doma(assigns) do
+    ~H"""
+    <style>
+      .general{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        padding: 60px;
+        background: linear-gradient(1deg, #f0ccb8, #ffffff3c);
+        background-size: 400% 400%;
+
+        -webkit-animation: VyletyGradient 6s ease infinite;
+        -moz-animation: VyletyGradient 6s ease infinite;
+        animation: VyletyGradient 6s ease infinite;
+      }
+      @-webkit-keyframes VyletyGradient {
+      0%{background-position:0% 23%}
+      50%{background-position:100% 78%}
+      100%{background-position:0% 23%}
+      }
+      @-moz-keyframes VyletyGradient {
+          0%{background-position:0% 23%}
+          50%{background-position:100% 78%}
+          100%{background-position:0% 23%}
+      }
+      @keyframes VyletyGradient {
+          0%{background-position:0% 23%}
+          50%{background-position:100% 78%}
+          100%{background-position:0% 23%}
+      }
+    </style>
+    <div class="general">
+      <div class="projekt"><h3>O PROJEKTU</h3><p>Hledáte doučování pro vaše dítě na předmět, který mu dělá problémy? Vidíte , že mu je školní hodina krátká a chce se ještě více zdokonalit v tom, co ho baví? Nebo se chce vzdělávat v tom, co školní osnovy neobsahují? Je tady jedinečná webová a mobilní aplikace, která vám zásadně pomůže a umožní vám najít rychle a přehledně toho správného pedagoga!</p></div>
+      <div class="prubeh"><p>Chcete soukromou výuku online nebo být součástí online třídy? Chcete osobní návštěvu učitele  přímo u vás doma nebo preferujete setkání na jiném místě? Stačí si vybrat!</p></div>
+      <div class="program"><p>Jste kvalifikovaný učitel, co miluje svou práci? Chcete nám pomoci budovat díky kvalitnějšímu a snadnějšímu vzdělávání lepší svět? Přidejte se k nám a společně umožněme dětem růst!</p></div>
     </div>
     """
   end
