a23c5372417224627f6def42646ebf2a335a9767
jorikchase <jorikchase@gmail.com>
2023-11-21 00:24:58 +0100

    simplyfied

--- lib/flashtones_web/components/ui components/zs/deti/detail-cta.ex
+++ lib/flashtones_web/components/ui components/zs/deti/detail-cta.ex
@@ -11,7 +11,7 @@ defmodule DetailCta do
       <span>Cena:<bold>4 950 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -29,7 +29,7 @@ defmodule DetailCta do
     </style>
     <div class="detail-cta" id="detail-cta">
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <p>Chci více informací:</p>
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -50,7 +50,7 @@ defmodule DetailCta do
       <span>Cena:<bold>4 950 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -68,7 +68,7 @@ defmodule DetailCta do
     </style>
     <div class="detail-cta" id="detail-cta">
       <a class="cta-button" href="/zs/plavani/rezervace">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <p>Chci více informací:</p>
       <br>
       <a href="tel:+420 606 589 786"><img src="/images/icon/call.svg">+420 606 589 786</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">info@zsplavani.cz</a>
@@ -89,7 +89,7 @@ defmodule DetailCta do
       <span>Cena:<bold>3 700 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -110,7 +110,7 @@ defmodule DetailCta do
       <span>Cena:<bold>2 850 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -131,7 +131,7 @@ defmodule DetailCta do
       <span>Cena:<bold>4 950 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -149,7 +149,7 @@ defmodule DetailCta do
     </style>
     <div class="detail-cta" id="detail-cta">
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <p>Chci více informací:</p>
       <br>
       <a href="tel:+420 606 589 786"><img src="/images/icon/call.svg">+420 606 589 786</a>
       <a href="mailto:info@zsprodeti.cz"><img src="/images/icon/email.svg">info@zsprodeti.cz</a>
@@ -170,7 +170,7 @@ defmodule DetailCta do
       <span>Cena:<bold>4 950 Kč</bold></span>
       <br>
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
@@ -188,7 +188,7 @@ defmodule DetailCta do
     </style>
     <div class="detail-cta" id="detail-cta">
       <a class="cta-button" href="https://rezervace.zsprodeti.cz/kurz-1/termin/list">Objednat kurz</a>
-      <p>Chci více informací o kurzu:</p>
+      <!--<p>Chci více informací o kurzu:</p>-->
       <br>
       <a href="tel:+420 724 168 962"><img src="/images/icon/call.svg">+420 724 168 962</a>
       <a href="mailto:petra@zsplavani.cz"><img src="/images/icon/email.svg">petra@zsplavani.cz</a>
