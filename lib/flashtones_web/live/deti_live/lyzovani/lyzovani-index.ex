51b6168ccbb9a79969b2514bbf156dd6114f641a
jorikchase <jorikchase@gmail.com>
2023-11-21 00:28:07 +0100

    design tweaks

--- lib/flashtones_web/live/deti_live/lyzovani/lyzovani-index.ex
+++ lib/flashtones_web/live/deti_live/lyzovani/lyzovani-index.ex
@@ -12,7 +12,7 @@ defmodule FlashtonesWeb.LyzovaniIndexLive do
         flex-direction: column;
         justify-content: space-evenly;
         background-position: fixed;
-        background: linear-gradient(1deg, #faf0cd, #cbd0f9, #c6f9c9, #f8c6c6, #f9e3cb, #ffffff3c);
+        background: linear-gradient(1deg, #cdfaf8, #cbddf9, #c6f7f9, #c6def8, #cbf0f9, #fffffff2);
         background-size: 400% 400%;
 
         -webkit-animation: MainGradient 20s ease-in-out infinite;
