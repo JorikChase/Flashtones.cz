95b4a0ca9b31def991c5e414a21457515b062db8
jorikchase <jorikchase@gmail.com>
2024-01-03 13:44:37 +0100

    database removed

--- config/dev.exs
+++ config/dev.exs
@@ -1,14 +1,14 @@
 import Config
 
 # Configure your database
-config :flashtones, Flashtones.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "flashtones_dev",
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+# config :flashtones, Flashtones.Repo,
+#   username: "postgres",
+#   password: "postgres",
+#   hostname: "localhost",
+#   database: "flashtones_dev",
+#   stacktrace: true,
+#   show_sensitive_data_on_connection_error: true,
+#   pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
