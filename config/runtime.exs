568510004e7abc4c7ccdc28117f1c13362274d5b
jorikchase <jorikchase@gmail.com>
2024-01-06 17:23:41 +0100

    produkty

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,7 @@
 import Config
 
+config :flashtones, KEYFILE: "/etc/letsencrypt/live/www.zsenviro.cz/privkey.pem"
+config :flashtones, CERTFILE: "/etc/letsencrypt/live/www.zsenviro.cz/fullchain.pem"
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
