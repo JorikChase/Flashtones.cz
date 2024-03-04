b8cddd52206f523b890b347b23f69abfb5037a80
jorikchase <jorikchase@gmail.com>
2024-03-04 11:54:12 +0100

    topbar color

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,7 @@ Hooks.ModularMenuRight = ModularMenuRight
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({barColors: {0: "#d8b770"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
