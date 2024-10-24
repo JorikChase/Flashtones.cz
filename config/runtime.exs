8b441d193b0dd04b4d87f61556f67bd38b6fc31c
JorikChase <71600520+JorikChase@users.noreply.github.com>
2024-10-25 00:10:18 +0200

    port hardcoded schism

--- config/runtime.exs
+++ config/runtime.exs
@@ -42,7 +42,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000" || "4001")
+  port = String.to_integer(System.get_env("PORT"))
 
   config :flashtones, FlashtonesWeb.Endpoint,
     url: [host: host, port: 4000, scheme: "http"],
@@ -52,7 +52,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 1},
-      port: port
+      port: 4000
     ],
     secret_key_base: secret_key_base
 
@@ -64,7 +64,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 1},
-      port: port
+      port: 4001
     ],
     secret_key_base: secret_key_base
 
