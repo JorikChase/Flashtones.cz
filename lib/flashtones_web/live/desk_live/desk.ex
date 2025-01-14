0907854c575dd401d70a60b42ba6e8340f842b3d
JorikChase <71600520+JorikChase@users.noreply.github.com>
2025-01-14 09:44:45 +0100

    Update desk.ex

--- lib/flashtones_web/live/desk_live/desk.ex
+++ lib/flashtones_web/live/desk_live/desk.ex
@@ -18,10 +18,10 @@ defmodule FlashtonesWeb.DeskLive do
     ~H"""
     <script type="importmap">
       {
-      "imports": {
-          "three": "https://unpkg.com/three@0.162.0/build/three.module.js",
-          "three/addons/": "https://unpkg.com/three@0.162.0/examples/jsm/"
-      }
+        "imports": {
+          "three": "https://cdn.jsdelivr.net/npm/three@0.162.0/build/three.module.js",
+          "three/examples/": "https://cdn.jsdelivr.net/npm/three@0.162.0/examples/jsm/"
+        }
       }
     </script>
 
