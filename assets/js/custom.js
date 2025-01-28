c1e98aa5dd2bcf86ed19058b3fdd95f5ea4709a8
JorikChase <71600520+JorikChase@users.noreply.github.com>
2025-01-28 10:28:20 +0100

    tag

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -304,22 +304,20 @@ export const ModularMenuRight = {
     }
 
     function enableGoogleAnalytics() {
-      // Embedding the Google Tag Manager code
-      (function () {
-        var script = document.createElement("script");
-        script.async = true;
-        script.src =
-          "https://www.googletagmanager.com/gtag/js?id=AW-11418638935";
-        document.head.appendChild(script);
-        console.log("tag active");
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        window.gtag = gtag; // Make gtag globally available
-        gtag("js", new Date());
-        gtag("config", "G-FKH8YGWNRR");
-      })();
+      // Create and append the first script element
+      const firstScript = document.createElement("script");
+      firstScript.async = true;
+      firstScript.src =
+        "https://www.googletagmanager.com/gtag/js?id=G-SYGJRGDW2D";
+      document.head.appendChild(firstScript);
+
+      // Initialize dataLayer and gtag function
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-SYGJRGDW2D");
     }
 
     // Function containing your modulate logic
