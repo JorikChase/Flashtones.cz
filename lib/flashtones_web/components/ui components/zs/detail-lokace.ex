f88b08c2183d99f260b696936d1dc874356b4a94
sseerrppeenntt <sseerrppeenntt@proton.me>
2025-03-04 13:36:19 +0100

    Uprava uprav od Petry

--- lib/flashtones_web/components/ui components/zs/detail-lokace.ex
+++ lib/flashtones_web/components/ui components/zs/detail-lokace.ex
@@ -1436,7 +1436,7 @@ defmodule DetailLokace do
                             <div class="product-text-wrap">
                             <div class="product-text">
                             <span class="cost-wrap"><h4>
-                            Plavecký bazén Gymnázium Paměti Národa
+                            Gymnázium Paměti Národa
                             </h4><b>4 250 <side>Kč</side></b></span>
                             <p class="font-normal">Příměstský tábor ZŠ PRO DĚTI se bude konat v jedinečné lokalitě Gymnázia Paměti Národa na Praze 5, které nabízí ideální prostředí pro všechny naše aktivity. Tento moderní areál je nejen bohatý na historickou atmosféru, ale také vybavený kvalitními venkovními prostory, jako jsou prostorná hřiště a zelené plochy, které umožňují pestré sportovní a outdoorové vyžití. Dětem poskytne bezpečné zázemí pro všechny táborové činnosti a současně je inspiruje k objevování a aktivnímu pohybu v krásném prostředí.</p>
                             </div>
