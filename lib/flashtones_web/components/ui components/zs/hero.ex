1f082f37ab8ed01c824409da1384466905076a74
sseerrppeenntt <sseerrppeenntt@proton.me>
2025-04-15 13:47:26 +0200

    Pridane odsazeni mobilniho menicka pro prehlednost

--- lib/flashtones_web/components/ui components/zs/hero.ex
+++ lib/flashtones_web/components/ui components/zs/hero.ex
@@ -1035,6 +1035,9 @@ defmodule Hero do
       }
 
       @media (orientation: portrait) {
+      .instruktori-hero {
+        margin-top: 60px;
+      }
       .instruktori-hero-footer {
       background: transparent;
       padding: 0;
